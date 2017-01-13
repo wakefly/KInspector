8ce35800a9f72fb3a07facfe74b8e3a14a504a14
Christopher Jennings <ChrisJ@Kentico.com>
2017-01-13 16:12:31 -0500

    Initial commit of (broken) proejcts

---
+++ KInspector.Desktop/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Kentico.KInspector.WebApplication;
+
+namespace Kentico.KInspector.Desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static IDisposable WebApplicationServer;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            WebApplicationServer = Microsoft.Owin.Hosting.WebApp.Start<Startup>("http://localhost:9000");
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            WebApplicationServer?.Dispose();
+            base.OnExit(e);
+        }
+    }
+}
---
+++ KInspector.WebApplication/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Web.Http;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using Microsoft.Owin.StaticFiles;
+using Microsoft.Owin.FileSystems;
+
+[assembly: OwinStartup(typeof(Kentico.KInspector.WebApplication.Startup))]
+
+namespace Kentico.KInspector.WebApplication
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Cors is needed to request localhost server from filesystem
+            appBuilder.UseCors(CorsOptions.AllowAll);
+
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            var physicalFileSystem = new PhysicalFileSystem(@".\FrontEnd"); //. = root, Web = your physical directory that contains all other static content, see prev step
+            var options = new FileServerOptions
+            {
+                EnableDefaultFiles = true,
+                FileSystem = physicalFileSystem
+            };
+            options.StaticFileOptions.FileSystem = physicalFileSystem;
+            options.StaticFileOptions.ServeUnknownFileTypes = true;
+            options.DefaultFilesOptions.DefaultFileNames = new[] { "index.html" }; //put whatever default pages you like here
+            appBuilder.UseFileServer(options);
+
+            //appBuilder.UseStaticFiles(staticFileOptions);
+            //appBuilder.UseStaticFiles("/FrontEnd");
+            appBuilder.UseWebApi(config);
+
+            //appBuilder.Run(context =>
+            //{
+            //    context.Response.ContentType = "text/plain";
+            //    return context.Response.WriteAsync("Hello, world.");
+            //});
+        }
+    }
+}
