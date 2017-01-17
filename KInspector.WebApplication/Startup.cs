40b62ab3f1366ae3c74646b506601af1649fe8bd
Christopher Jennings <ChrisJ@Kentico.com>
2017-01-17 13:31:58 -0500

    Simplify and cleanup Startup class

--- KInspector.WebApplication/Startup.cs
+++ KInspector.WebApplication/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Cors;
 using Owin;
 using Microsoft.Owin.StaticFiles;
 using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles.ContentTypes;
 
 [assembly: OwinStartup(typeof(Kentico.KInspector.WebApplication.Startup))]
 
@@ -18,6 +19,26 @@ namespace Kentico.KInspector.WebApplication
             // Cors is needed to request localhost server from filesystem
             appBuilder.UseCors(CorsOptions.AllowAll);
 
+            ConfigureWebApi(appBuilder);
+
+            ConfigureStaticFileServing(appBuilder);
+        }
+
+        private static void ConfigureStaticFileServing(IAppBuilder appBuilder)
+        {
+            var physicalFileSystem = new PhysicalFileSystem(@".\www");
+
+            var options = new FileServerOptions
+            {
+                EnableDefaultFiles = true,
+                FileSystem = physicalFileSystem
+            };
+            
+            appBuilder.UseFileServer(options);
+        }
+
+        private static void ConfigureWebApi(IAppBuilder appBuilder)
+        {
             HttpConfiguration config = new HttpConfiguration();
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
@@ -25,26 +46,7 @@ namespace Kentico.KInspector.WebApplication
                 defaults: new { id = RouteParameter.Optional }
             );
 
-            var physicalFileSystem = new PhysicalFileSystem(@".\www"); //. = root, Web = your physical directory that contains all other static content, see prev step
-            var options = new FileServerOptions
-            {
-                EnableDefaultFiles = true,
-                FileSystem = physicalFileSystem
-            };
-            options.StaticFileOptions.FileSystem = physicalFileSystem;
-            options.StaticFileOptions.ServeUnknownFileTypes = true;
-            options.DefaultFilesOptions.DefaultFileNames = new[] { "index.html" }; //put whatever default pages you like here
-            appBuilder.UseFileServer(options);
-
-            //appBuilder.UseStaticFiles(staticFileOptions);
-            //appBuilder.UseStaticFiles("/FrontEnd");
             appBuilder.UseWebApi(config);
-
-            //appBuilder.Run(context =>
-            //{
-            //    context.Response.ContentType = "text/plain";
-            //    return context.Response.WriteAsync("Hello, world.");
-            //});
         }
     }
 }
