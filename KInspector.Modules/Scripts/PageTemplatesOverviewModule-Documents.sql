dc63d37bbb837579ab03dc09b05b8e386e194d38
Christopher Jennings <chrisj@kentico.com>
2017-10-19 17:04:14 -0400

    Update Document SQL to provide count rather than list all pages.

--- KInspector.Modules/Scripts/PageTemplatesOverviewModule-Documents.sql
+++ KInspector.Modules/Scripts/PageTemplatesOverviewModule-Documents.sql
@@ -1,5 +1,6 @@
-﻿SELECT S.SiteName, NodeAliasPath, DocumentCulture 
+﻿SELECT S.SiteName, DocumentCulture, Count(*)
 FROM View_CMS_Tree_Joined AS V
 INNER JOIN CMS_Site AS S on S.SiteID = V.NodeSiteID
 WHERE DocumentPageTemplateID = @PageTemplateID 
+GROUP BY S.SiteName, DocumentPageTemplateID, DocumentCulture
 ORDER BY S.SiteName, DocumentCulture
